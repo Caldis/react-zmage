34b9f6bf9413638cf9f524d03edb28427466562a
Caldis <532607446@qq.com>
2018-03-12 00:33:23 +0800

    移除部分说明 | Content update

--- docs/index.js
+++ docs/index.js
@@ -43,7 +43,7 @@ export default class ReactZmage extends React.Component {
                         </div>
 
                         <h1>react-zmage</h1>
-                        <h4>一个基于 React 的可缩放图片控件, 您可以用这个控件完全替代原生的 img 标签, 令其附带图片缩放功能</h4>
+                        <h4>一个基于 React 的可缩放图片控件, 您可以用这个控件替代原生的 img 标签, 令其附带图片缩放功能</h4>
 
                         <div className={style.button} onClick={() => window.open("https://github.com/Caldis/react-zmage")}>马上使用</div>
                     </div>
@@ -61,7 +61,6 @@ export default class ReactZmage extends React.Component {
                         <h2>基本用法</h2>
                         <p>一如原生的 {"<img/>"} 标签, 只需要直接替换 img 为 Zmage 即可, 您依旧可以使用 style, className 等原生属性。</p>
                         <p>现在, 点击图片, 您即可进入<b>查看模式</b>。</p>
-                        <p>注意到了吗? 您传入的 alt 属性将会在放大后作为标题显示在全屏状态的左上角。</p>
 	                    <pre>
 <PrismCode className="language-javascript">
 {`<Zmage
@@ -84,7 +83,7 @@ export default class ReactZmage extends React.Component {
                 <div className={style.horizon}>
                     <div>
                         <h2>图片放大</h2>
-                        <p>注意到了吗, 在放大后的图片右上角有一个奇怪的图标, 不妨点击试试 ?</p>
+                        <p>点击右上角的放大图标, 即可进入放大模式。</p>
                         <p>滑动鼠标则可以浏览超出屏幕的部分, 对浏览大尺寸图片时尤为方便。</p>
                         <p>在<b>放大模式</b>中, 图片会设置为 100%放大, 确保不会有任何失真。</p>
                         <p>再次点击屏幕即可退出<b>放大模式</b>。</p>
@@ -110,15 +109,14 @@ export default class ReactZmage extends React.Component {
                 <div className={style.horizon}>
                     <div>
                         <h2>展示序列图片</h2>
-                        <p>您可以传入一系列图片来显示一个幻灯片, 使用 <b>imageSet</b> 来包裹它们。</p>
-                        <p>点击放大后使用键盘的 <b>←</b> 或 <b>→</b> , 或点击屏幕两侧的按钮即可切换图片。</p>
-                        <p>左右两侧的控制按钮仅会在您提供了有效的 <b>imageSet</b> 参数时才会显示</p>
+                        <p>您可以传入一系列图片来显示一系列幻灯片, 使用 <b>set</b> 来包裹它们。</p>
+                        <p>点击放大后使用键盘的 <b>←</b> 或 <b>→</b> 即可切换图片。</p>
                         <pre>
 <PrismCode className="language-javascript">
 {`<Zmage
     src="your.cover.image.link.jpg"
     alt="展示序列图片"
-    imageSet={[{
+    set={[{
         src: "your.cover.image.link.jpg",
         alt: "First image description"
     },{
@@ -134,11 +132,11 @@ export default class ReactZmage extends React.Component {
                             className={style.image}
                             src="imgSet/aboutDeer/3.jpg"
                             alt="展示序列图片"
-                            imageSet={[{
+                            set={[{
                                 src: "imgSet/aboutDeer/3.jpg",
                                 alt: "關於鹿 · THREE"
                             },{
-                                src: "imgSet/aboutDeer/4.jpg",
+                                src: "https://rpearce.github.io/react-medium-image-zoom/nz-big.jpg",
                                 alt: "關於鹿 · FOUR"
                             },{
                                 src: "imgSet/aboutDeer/5.jpg",
@@ -162,7 +160,7 @@ export default class ReactZmage extends React.Component {
                             className={style.image}
                             src="imgSet/aboutDeer/6.jpg"
                             alt="使用键盘进行快捷操作"
-                            imageSet={[{
+                            set={[{
 	                            src: "imgSet/aboutDeer/6.jpg",
 	                            alt: "關於鹿 · SIX"
                             },{
@@ -180,40 +178,6 @@ export default class ReactZmage extends React.Component {
                             }}
                         />
                         <a href="https://www.behance.net/gallery/58036101/_">via. sslololss Guihuahuzi on behance</a>
-                    </div>
-                </div>
-
-                <div className={style.horizon}>
-                    <div>
-                        <h2>函数式调用</h2>
-                        <p>如果您不需要一个封面图片, 您可以手动调用 <b>showImage()</b> 方法来直接呼出查看模式</p>
-                        <p>您只需要提供一个 <b>imageSet</b> 参数即可, 使用方式就如同直接从封面展示序列图片一样</p>
-                        <p>如果您只需要展示一张图片, 您可以直接传入一个包含 <b>src</b> 与 <b>alt</b> 元素的对象</p>
-                        <pre>
-<PrismCode className="language-javascript">
-{`onClick={() => showImage([
-    src: "your.cover.image.link.jpg",
-    alt: "First image description"
-},{
-    src: "your.another.image.link.jpg",
-    alt: "Second image description"
-}])`}
-</PrismCode>
-                        </pre>
-                    </div>
-                    <div style={{ textAlign: 'center' }}>
-                        <div
-                            className={style.button}
-                            onClick={() => showImage({
-                                imageSet: [{
-                                    src: "imgSet/aboutDeer/9.jpg",
-                                    alt: "關於鹿 · NINE"
-                                },{
-                                    src: "imgSet/aboutDeer/10.jpg",
-                                    alt: "關於鹿 · TEN"
-                                }]
-                            })}
-                        >呼出查看模式</div>
                     </div>
                 </div>
 
