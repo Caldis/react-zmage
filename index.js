969a5e5fb22c1a6a8c6d449e471c06e482cc4afc
Cyrus <532607446@qq.com>
2018-11-28 22:27:25 +0800

    更新发布逻辑 | Update publish rules

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./lib/zmage.production.min.js');
-} else {
-    module.exports = require('./lib/zmage.development.js');
-}
