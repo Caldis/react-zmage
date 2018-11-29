1f34945ddda36b045a73044798324d329cdb1abb
Cyrus <532607446@qq.com>
2018-11-30 01:50:19 +0800

    添加默认属性 | Add default props

--- src/config/default.js
+++ src/config/default.js
@@ -1,13 +1,133 @@
 /**
- * 定义默认值与类型
+ * 默认类型与默认值
  **/
 
-// React Libs
+// Libs
 import PropTypes from 'prop-types';
 
-// 默认值
+/**
+ * 默认类型
+ **/
+export const defType = {
+
+    /**
+     * 基础数据
+     **/
+    // 图片地址
+    src: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func,
+    ]).isRequire,
+    // 图片标题
+    alt: PropTypes.string,
+    // 图片描述
+    txt: PropTypes.string,
+    // 图片集合
+    set: PropTypes.oneOfType([
+        PropTypes.arrayOf(
+            PropTypes.shape({
+                src: PropTypes.string,  // 图片地址
+                alt: PropTypes.string,  // 图片标题
+                text: PropTypes.string, // 图片描述
+            })
+        ),
+        PropTypes.shape({
+            src: PropTypes.string,  // 图片地址
+            alt: PropTypes.string,  // 图片标题
+            text: PropTypes.string, // 图片描述
+        }),
+    ]),
+
+    /**
+     * 功能控制
+     **/
+    // 控制器
+    controller: PropTypes.oneOfType([
+        PropTypes.bool,
+        PropTypes.shape({
+            // 分页
+            pagination: PropTypes.bool,
+            // 标题
+            title: PropTypes.bool,
+            // 旋转按钮
+            rotate: PropTypes.bool,
+            // 缩放按钮
+            zoom: PropTypes.bool,
+            // 关闭按钮
+            close: PropTypes.bool,
+            // 左右翻页
+            flip: PropTypes.bool,
+        }),
+    ]),
+    // 快捷键
+    hotKey: PropTypes.shape({
+        // 缩放（空格）
+        zoom: PropTypes.bool,
+        // 关闭（ESC）
+        close: PropTypes.bool,
+        // 翻页（左右键）
+        flip: PropTypes.bool,
+    }),
+    // 移动端
+    mobile: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.bool,
+    ]),
+
+    /**
+     * 界面样式
+     **/
+    // 背景色
+    backdrop: PropTypes.string,
+    // 高度
+    zIndex: PropTypes.number,
+    // 边距
+    edge: PropTypes.number,
+
+    /**
+     * 生命周期
+     **/
+    onBrowsing: PropTypes.func,
+    onZooming: PropTypes.func,
+    onSwitching: PropTypes.func,
+    onRotating: PropTypes.func,
+
+    /**
+     * 杂项
+     **/
+    // 传导信息
+    cover: PropTypes.object,
+    remove: PropTypes.func,
+    // 动画参数
+    springOption: PropTypes.shape({
+        // 剛性
+        stiffness: PropTypes.number,
+        // 阻尼
+        damping: PropTypes.number,
+    }),
+    getSpringOption: PropTypes.func,
+}
+
+/**
+ * 默认值
+ **/
 export const defProp = {
 
+    /**
+     * 基础数据
+     **/
+    // 图片地址
+    src: "",
+    // 图片标题
+    alt: "",
+    // 图片描述
+    txt: "",
+    // 图片集合
+    set: [],
+	
+    /**
+     * 功能控制
+     **/
 	// 控制器
 	controller: {
 		// 分页
@@ -23,7 +143,6 @@ export const defProp = {
 		// 左右翻页
 		flip: true,
 	},
-
 	// 快捷键
 	hotKey: {
 		// 缩放（空格）
@@ -33,79 +152,36 @@ export const defProp = {
 		// 翻页（左右）
 		flip: true,
 	},
+    // 移动端
+    mobile: "auto",
 
-    // 样式相关
+    
+    /**
+     * 界面样式
+     **/
     // 背景色
     backdrop: "#FFFFFF",
     // 高度
     zIndex: 1000,
+    // 边距
+    edge: 20,
+	
+    /**
+     * 生命周期
+     **/
+    onBrowsing: ()=>{},
+    onZooming: ()=>{},
+    onSwitching: ()=>{},
+    onRotating: ()=>{},
 
-    // 杂项
-    // 图片距屏幕边距 (如果有)
-    margin: 70,
+    /**
+     * 杂项
+     **/
+    // 传导信息
+    cover: {},
+    remove: ()=>{},
 	// 动画参数
     springOption: { stiffness: 230, damping: 25, precision: 0.01 },
     getSpringOption: (precision) => ({ stiffness: 230, damping: 25, precision })
-
-}
-
-
-// 默认类型
-export const defType = {
-
-	// 图片集合
-    set: PropTypes.oneOfType([
-		PropTypes.arrayOf(PropTypes.shape({
-			src: PropTypes.string,  // 图片链接
-			alt: PropTypes.string,  // 同 img 标签的 alt
-			text: PropTypes.string, // 图片描述文字
-		})),
-		PropTypes.shape({
-			src: PropTypes.string,  // 图片链接
-			alt: PropTypes.string,  // 同 img 标签的 alt
-			text: PropTypes.string, // 图片描述文字
-		}),
-	]),
-
-	// 控制器
-	controller: PropTypes.shape({
-		// 分页
-		pagination: PropTypes.bool,
-		// 标题
-		title: PropTypes.bool,
-        // 旋转按钮
-        rotate: PropTypes.bool,
-		// 缩放按钮
-		zoom: PropTypes.bool,
-        // 关闭按钮
-        close: PropTypes.bool,
-		// 左右翻页
-		flip: PropTypes.bool,
-	}),
-
-	// 快捷键
-	hotKey: PropTypes.shape({
-		// 缩放（空格）
-		zoom: PropTypes.bool,
-        // 关闭（ESC）
-        close: PropTypes.bool,
-		// 翻页（左右键）
-		flip: PropTypes.bool,
-	}),
-
-    // 样式相关
-    // 背景色
-    backdrop: PropTypes.string,
-    // 高度
-    zIndex: PropTypes.number,
-
-    // 杂项
-    // 图片距屏幕边距 (如果有)
-    margin: PropTypes.number,
-    // 动画参数
-    springOption: PropTypes.shape({
-        stiffness: PropTypes.number, // 剛性
-        damping: PropTypes.number,   // 阻尼
-    }),
 
 }
