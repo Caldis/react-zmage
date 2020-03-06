6b33a7e6210bcc8b6abac708e00b6c54b41ae30c
Caldis <532607446@qq.com>
2020-03-07 01:20:40 +0800

    更新打包策略 | Update package script

--- scripts/webpack.lib.ssr.config.babel.js
+++ scripts/webpack.lib.ssr.config.babel.js
@@ -9,21 +9,30 @@ import merge from 'webpack-merge'
 import libBaseConfig from './webpack.lib.base.config.babel.js'
 // Plugins
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-import TerserJSPlugin from 'terser-webpack-plugin';
+import TerserJSPlugin from 'terser-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
 
 const config = merge.smart(libBaseConfig({ ssr:true }), {
 
     mode: "production",
 
     output: {
-        filename: 'zmage.production.ssr.js',
+        // 从 output.path (lib) 目录开始查找
+        filename: '../ssr/zmage.production.ssr.js',
     },
 
     plugins: [
         // 样式文件分离
-        new MiniCssExtractPlugin({ filename:'zmage.production.ssr.css' }),
+        // 从 output.path (lib) 目录开始查找
+        new MiniCssExtractPlugin({ filename:'../ssr/style.css' }),
+        // 复制类型文件
+        // from 从项目根目录开始查找
+        // to 从 from 目录开始查找
+        new CopyPlugin([
+            { from: 'lib/index.d.ts', to: '../ssr/index.d.ts' }
+        ]),
     ],
 
     optimization: {
